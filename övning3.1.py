62849c29093bd942d812173a5257e3d7d47fa38d
Luigih123 <luigi.hansson@elev.ga.ntig.se>
2020-10-19 12:37:56 +0200

    Update övning3.1.py

--- övning3.1.py
+++ övning3.1.py
@@ -1,7 +1,7 @@
-minuter = int(input('Hur många minuter ringer du per månad?'))
-if minuter <= 32:
+minuter = int(input('Hur många minuter ringer du per månad?')) # Här frågar jag användaren hur många minuter dem ringer totalt i månaden
+if minuter <= 32: # Här räknar den ut om det är mindre eller likamed 32 så får du ut svaret kontant
     print('Kontant')
-elif minuter >= 67:
+elif minuter >= 67: # Här räknar den ut om det är mer eller likamed 67 så får du ut svaret plus 
         print('plus')
-else:
+else: # Här så räkner den om du har skrivit ett tal mellan 33 till 66 och då får du ut svaret normal
     print('normal')
